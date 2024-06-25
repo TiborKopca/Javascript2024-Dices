5e3717c7c4c8a76c6254d25a958af69a6cff7375
TiborKopca <tibor.kopca@gmail.com>
2024-06-25 23:45:51 +0200

    first commit - adding all files

---
+++ README.md
@@ -0,0 +1,9 @@
+# Simple project - Javascript - HTML - CSS
+* Random number generator with range slider
+
+## Screenshot
+![here](/screenshot.png)
+
+## Authors
+
+- [@TiborKopca](https://github.com/TiborKopca)
---
+++ js/app.js
@@ -0,0 +1,58 @@
+//CONSTANTES
+const randomBtn = document.getElementById("btn-random");
+const rangeBtn = document.getElementById("btn-range");
+const picture = document.getElementById("picture");
+const section = document.getElementById("centrar");
+const quantity = document.getElementById("dado__input-quantity");
+const rangevalue = document.getElementById("range-value");
+
+//CREATE ELEMENT
+
+
+
+
+function randomInt(numMin, numMax) {
+    return Math.floor(Math.random() * numMax) + numMin;
+}
+
+function tirarDadoAleatorio() {
+    const numMax = 6; //max 6 dados
+    const numMin = 1; //min 1 dado
+    //create element of your choice
+    const dado = document.createElement("img");
+    //cleaning all elements under the parent container picture
+    picture.innerHTML = "";
+    //call function to obtain a random number
+    let numero = randomInt(numMin, numMax);
+    //set the src of the element to the random number
+    dado.src = `./img/${numero}.png`;
+    //append the element to the parent container picture
+    picture.appendChild(dado);
+}
+
+function tirarDadoRange() {
+    //dados is the value of the range input
+    let dados = quantity.value;
+    //clear previus atempts
+    picture.innerHTML = "";
+    //loop to create the dices by the range input
+    for (let i = 1; i <= dados; i++) { 
+        const dado = document.createElement("img");
+        picture.insertBefore(dado, picture.firstChild);
+        dado.id = "dado";
+        dado.className = "dado";
+        dado.src = `./img/${randomInt(1, 6)}.png`;
+        // picture.insertBefore(dado, picture.children[0]);
+    }
+    
+    // dado.appendChild(newimage);
+}
+
+
+//EVENT LISTENERS
+randomBtn.addEventListener("click", tirarDadoAleatorio);
+rangeBtn.addEventListener("click", tirarDadoRange);
+
+quantity.addEventListener("input", () => {
+    rangevalue.textContent = quantity.value;
+});
